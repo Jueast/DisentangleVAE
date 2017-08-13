b77a86a1e8e3be745a398177abd6a35b46554a66
Jueast <jueastwang@outlook.com>
2017-08-13 17:23:03 +0800

    fix a int bug; add a mask for

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--ngpus', type=int, default=4)
@@ -52,7 +53,7 @@ elif args.model == 'MMDVAE':
     network = MMDVAE(dataset.data_dims, [args.dimz],
                       hidden=args.hidden, beta=args.beta)
 elif args.model == 'VLAE':
-    network = VLAE(dataset.data_dims, [args.hlayers, args.dimz], hidden=args.hidden, beta=args.beta)
+    network = VLAE(dataset.data_dims, [args.hlayers, int(args.dimz / args.hlayers)], hidden=args.hidden, beta=args.beta)
 else:
     print("Unknown model")
     exit(-1)
